e2f94ffc2033c26b7b5d6da517d788e8a5804a34
TimBock <tim.bock@numbers.net.au>
2017-01-27 18:50:33 +1100

    DS-998 Fixing bug in bagged k-means

--- tests/testthat/test-bugs.R
+++ tests/testthat/test-bugs.R
@@ -16,4 +16,31 @@ test_that("DS-975",
 
 
 
+#
+# test_that("DS-998",
+#           {
+#               library(foreign)
+#               suppressWarnings({
+#               test.data <- CreateSimulatedClusters(3, 4, 10, 500)
+#
+#                 z gss <- read.spss("http://docs.displayr.com/images/7/7b/GSS2014.sav", to.data.frame = TRUE)
+#                 gss <- flipExampleData::TidySPSS(gss)
+#                 attach(gss)
+#                 zd = data.frame(confinan, conbus, coneduc, conlabor, conmedic, conpress)
+#                 kmeans <- KMeans(zd,
+#                                  show.labels = TRUE,
+#                                  algorithm = "Bagging",
+#                                  missing = "Exclude cases with missing data")
+#                 n = 50
+# e1071::bclust(zd, iter.base = 100, weights = NULL, centers = 2, base.centers = min(20, n / 5), final.kmeans = FALSE, verbose = FALSE)
+#
+#                 expect_error(print(kmeans), NA)
+#                 detach(gss)
+#               })
+#           })
+
+
+
+
+
 
