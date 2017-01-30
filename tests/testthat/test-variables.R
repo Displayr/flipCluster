b8d289f648831f07ae32eb5f68331b16e4d7e043
TimBock <tim.bock@numbers.net.au>
2017-01-31 08:49:05 +1100

    Fixing error in unit tests for kmeans
    Matrix was being passed in that

--- tests/testthat/test-variables.R
+++ tests/testthat/test-variables.R
@@ -2,7 +2,7 @@ context("Allocate to clusters (predict)")
 
 test_that("Allocate to clusters",
           {
-              df <- cbind(c(1:4,1), NA)
+              df <- as.data.frame(cbind(c(1:4,1), NA))
               cl <- matrix(c(1:4, 4:1), 4, 2)
               expect_equal(predict.KMeans(cl, df), c(1,2,3,4,1))
               expect_equal(predict.KMeans(cl, df[, 2:1] ), c(4,3,2,1,4))
