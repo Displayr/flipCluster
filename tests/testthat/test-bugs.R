72711eeeb4f50ab4358ec27ab649b3934e41fb04
TimBock <tim.bock@numbers.net.au>
2017-05-02 08:38:18 +1000

    DS-1169: tidying up reading of an SPSS file

--- tests/testthat/test-bugs.R
+++ tests/testthat/test-bugs.R
@@ -4,7 +4,7 @@ test_that("DS-975",
           {
               library(foreign)
               suppressWarnings({
-                gss <- read.spss("http://docs.displayr.com/images/7/7b/GSS2014.sav", to.data.frame = TRUE)
+                gss <- read.spss("http://docs.displayr.com/images/7/7b/GSS2014.sav", to.data.frame = TRUE, max.value.labels = 20)
                 gss <- flipExampleData::TidySPSS(gss)
                 attach(gss)
                 kmeans <- KMeans(data.frame(confinan, conbus, coneduc, conlabor, conmedic, conpress), show.labels = TRUE)
